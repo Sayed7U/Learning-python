837a6186a81ca34d2e8b877a5e14ea49e5da81d9
Sayed7U <sayed98@hotmail.co.uk>
2020-05-13 13:51:19 +0100

    python 3 fixes

--- odd or even.py
+++ odd or even.py
@@ -1,11 +1,11 @@
 __author__ = 'Sayed'
-print "Welcome to odd or even number tester."
-number = int(raw_input("Type in number to test: "))
+print ("Welcome to odd or even number tester.")
+number = int(input("Type in number to test: "))
 
 while True:
     if number % 2 == 0:
-        print "Number is even."
-        number = int(raw_input("Type in number to test: "))
+        print("Number is even.")
+        number = int(input("Type in number to test: "))
     else:
-        print "Number is odd."
-        number = int(raw_input("Type in number to test: "))
+        print("Number is odd.")
+        number = int(input("Type in number to test: "))
