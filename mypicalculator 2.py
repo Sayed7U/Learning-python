a56927e9377797ee8236dfe6f2dd087381dbe9f6
Sayed7U <sayed98@hotmail.co.uk>
2020-05-17 15:56:16 +0100

    messing with objects

--- mypicalculator 2.py
+++ mypicalculator 2.py
@@ -3,9 +3,9 @@ import os
 from decimal import *
 getcontext().prec = 70
 print("Welcome to the python pi calculator v2")
-print 0.75
-print 4/3
-raw_input("Press enter to start the calculation. ")
+print(0.75)
+print(4/3)
+input("Press enter to start the calculation. ")
 x = Decimal(2)
 y = Decimal(3)
 z = Decimal(4)
@@ -13,12 +13,12 @@ pi = Decimal(3)
 
 while pi > 0:
     pi = Decimal(pi + (4/(x*y*z)))
-    print "pi = " + str(pi)
+    print ("pi = " + str(pi))
     x += 2
     y += 2
     z += 2
     pi = Decimal(pi - (4/(x*y*z)))
-    print "pi = " + str(pi)
+    print ("pi = " + str(pi))
     x += 2
     y += 2
     z += 2
---
+++ user.py
@@ -0,0 +1,28 @@
+
+import datetime
+
+class User:
+    """A member, storing name and birthday
+    for now """
+    def __init__(self, full_name, birthday):
+        self.name = full_name
+        self.birthday = birthday ##yyyymmdd
+
+        name_pieces = full_name.split(" ")
+        self.first_name = name_pieces[0]
+        self.last_name = name_pieces[-1]
+    def age(self):
+        """Returns the age of the user in years"""
+        yyyy = int(self.birthday[0:4])
+        mm = int(self.birthday[4:6])
+        dd = int(self.birthday[6:8])
+        today = datetime.date.today()
+        dob = datetime.date(yyyy,mm,dd)
+        age_in_days = (today - dob).days
+        return int(age_in_days / 365)
+
+        
+user = User("Dave Bowman", "19980831")
+print(user.name)
+print(user.first_name)
+print(user.age())
